4a97b1cfc1128ab2d840f576cd76115947f36e15
Vtek <pontoreau.sylvain@gmail.com>
2017-12-05 14:34:23 +0100

    Add IValidator interface

---
+++ src/Gyoza/IValidator.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Gyoza
+{
+    /// <summary>
+    /// Define a message validator
+    /// </summary>
+    public interface IValidator<in TMessage>
+        where TMessage : IMessage
+    {
+        /// <summary>
+        /// Validate a message
+        /// </summary>
+        /// <param name="message">Message to validate</param>
+        /// <returns>Validation error messages</returns>
+        IEnumerable<(string, string)> Validate(TMessage message);
+    }
+}
