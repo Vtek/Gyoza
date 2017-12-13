2b3bc5963145ada5f6733a264330eef1acef3d32
Vtek <pontoreau.sylvain@gmail.com>
2017-12-13 16:16:04 +0100

    Dispatcher code documentation

--- src/Gyoza/Dispatcher.cs
+++ src/Gyoza/Dispatcher.cs
@@ -4,15 +4,30 @@ using System.Threading.Tasks;
 
 namespace Gyoza
 {
+    /// <summary>
+    /// Message dispatcher
+    /// </summary>
     public class Dispatcher : IDispatcher
     {
+        /// <summary>
+        /// Service provider
+        /// </summary>
         private IServiceProvider ServiceProvider { get; }
 
+        /// <summary>
+        /// Create a new instance of Dispatcher class
+        /// </summary>
+        /// <param name="serviceProvider">Service provider, need to be injected</param>
         public Dispatcher(IServiceProvider serviceProvider)
         {
             ServiceProvider = serviceProvider;
         }
 
+        /// <summary>
+        /// Dispatch a message asynchronously
+        /// </summary>
+        /// <param name="message">Message to dispatch</param>
+        /// <returns>Dispatch result</returns>
         async Task<IResult> IDispatcher.DispatchAsync<TMessage>(TMessage message)
         {
             var validator = ServiceProvider.GetService(typeof(IValidator<TMessage>)) as IValidator<TMessage>;
--- src/Gyoza/IDispatcher.cs
+++ src/Gyoza/IDispatcher.cs
@@ -2,8 +2,16 @@ using System.Threading.Tasks;
 
 namespace Gyoza
 {
+    /// <summary>
+    /// Define a message dispatcher
+    /// </summary>
     public interface IDispatcher
     {
+        /// <summary>
+        /// Dispatch a message
+        /// </summary>
+        /// <param name="message">Message to dispatch</param>
+        /// <returns>Dispatch result</returns>
         Task<IResult> DispatchAsync<TMessage>(TMessage message)
             where TMessage : IMessage;
     }
